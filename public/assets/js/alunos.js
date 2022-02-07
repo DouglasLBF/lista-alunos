7c7cf8af362033d898509288fcb36c3217f4557d
carkarof <douglaslaza@gmail.com>
2022-02-07 14:53:51 -0300

    tentativa de pegar url

--- public/assets/js/alunos.js
+++ public/assets/js/alunos.js
@@ -9,12 +9,21 @@ $(document).ready(function() {
   // Primeiro ano a ser mostrado 
    startYear:2019,
   // Ultimo ano a ser mostrado 
-   endYear:$anoatual,
-  
-   onchange:(function(){ })
-  });
-});     
+   endYear:$anoatual,   
+  }); 
+}); 
 
+function filtro(idescola){
+  console.log()
+  // let url = 'test.php';
+  
+  //let data = idescola
+
+    // $.get(url,data, (response) => {       
+        
+    //     // window.open(response, '_blank');
+    // });
+}
 
 
 
